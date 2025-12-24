5e8a6ec2c6d1ece9ac2dc0036cba9291d944e8f2
Emil Z <istarnightstudios@gmail.com>
2025-12-24 08:39:05 -0800

    Sync to public

--- SyncAndPush.bat
+++ SyncAndPush.bat
@@ -3,7 +3,7 @@ echo --- SYNCING TO PUBLIC ---
 powershell -ExecutionPolicy Bypass -File "./ReleaseTools.ps1"
 
 echo --- PUSHING PUBLIC REPO ---
-cd /d "D:\Repos\EMILtools-Public"
+cd /d "D:\Repos\EMILtools-Public\EMILtools"
 git add .
 git commit -m "Auto-sync from Private"
 git push origin main
