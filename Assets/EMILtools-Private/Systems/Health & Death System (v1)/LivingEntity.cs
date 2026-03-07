c1f622d6297cf9ed65c41d4714aaeff675add514
Emil Z <damienzemanek@lcad.edu>
2026-03-06 17:29:49 -0800

    File Structure Changes 2

--- Assets/EMILtools-Private/Systems/Health & Death System (v1)/LivingEntity.cs
+++ Assets/EMILtools-Private/Systems/Health & Death System (v1)/LivingEntity.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using EMILtools_Private.Testing.Disabler;
 using EMILtools.Core;
 using EMILtools.Extensions;
 using Sirenix.OdinInspector;
